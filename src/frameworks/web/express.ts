879587c796ea4360eb5fb4302d218f74fd0839cb
Rizwan <mhdrizwanpkd@gmail.com>
2024-12-26 10:32:13 +0530

    last commit

--- src/frameworks/web/express.ts
+++ src/frameworks/web/express.ts
@@ -1,6 +1,6 @@
 import express, { Application } from 'express'
 import cors from 'cors'
-
+import morgan from 'morgan'
 import cookieParser from 'cookie-parser'
 import router from './routes/router';
 import { errorHandler } from '../../adapters/middleware/errorHandler.middleware';
@@ -11,6 +11,7 @@ const expressConfig = (app: Application) => {
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use(cookieParser());
+    app.use(morgan('tiny'));
     app.use('/',router);
     app.use(errorHandler)
 }
