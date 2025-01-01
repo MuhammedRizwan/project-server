0a5aa5a49534faf060d5d18b0225532c98ec24c7
Rizwan <mhdrizwanpkd@gmail.com>
2025-01-01 17:50:15 +0530

    ignored api log

--- src/frameworks/web/express.ts
+++ src/frameworks/web/express.ts
@@ -12,7 +12,7 @@ const expressConfig = (app: Application) => {
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use(cookieParser());
-    app.use(morgan('combined', { stream: accessLogStream }));
+    app.use(morgan('tiny', { stream: accessLogStream }));
     app.use('/',router);
     app.use(errorHandler)
 }
--- src/frameworks/web/morgan.ts
+++ src/frameworks/web/morgan.ts
@@ -1,4 +1,4 @@
 import fs from 'fs'
 import path from 'path'
 
-export const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+export const accessLogStream = fs.createWriteStream(path.join('access.log'), { flags: 'a' });
